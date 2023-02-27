828ab0ae28ae44f24a694b865559bc714919083f
Chris Dalby <chris.dalby-cic-uk@uk.ibm.com>
2023-02-27 19:45:03 +0000

    added a file for minio file get

--- receive.py
+++ receive.py
@@ -12,25 +12,11 @@ def main():
     
     channel.basic_consume(queue='unpacker-queue',
                       auto_ack=True,
-                      on_message_callback=on_message)
+                      on_message_callback=callback)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
 
-def on_message(self, _unused_channel, basic_deliver, properties, body):
-    """Invoked by pika when a message is delivered from RabbitMQ. The
-    channel is passed for your convenience. The basic_deliver object that
-    is passed in carries the exchange, routing key, delivery tag and
-    a redelivered flag for the message. The properties passed in is an
-    instance of BasicProperties with the message properties and the body
-    is the message that was sent.
-    :param pika.channel.Channel _unused_channel: The channel object
-    :param pika.Spec.Basic.Deliver: basic_deliver method
-    :param pika.Spec.BasicProperties: properties
-    :param bytes body: The message body
-    """
-    print('Received message # %s from %s: %s',
-                basic_deliver.delivery_tag, properties.app_id, body)
 
 if __name__ == '__main__':
     try:
