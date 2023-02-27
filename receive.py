201a6a32af28af43b21766654e665ad2fbe71f05
Chris Dalby <chris.dalby-cic-uk@uk.ibm.com>
2023-02-27 19:32:27 +0000

    added a file for minio file get

--- receive.py
+++ receive.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 import pika, sys, os
+import functools
+import logging
+import time
+
+LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) '
+              '-35s %(lineno) -5d: %(message)s')
+LOGGER = logging.getLogger(__name__)
 
 def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
@@ -12,10 +19,25 @@ def main():
     
     channel.basic_consume(queue='unpacker-queue',
                       auto_ack=True,
-                      on_message_callback=callback)
+                      on_message_callback=on_message)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
+
+def on_message(self, _unused_channel, basic_deliver, properties, body):
+    """Invoked by pika when a message is delivered from RabbitMQ. The
+    channel is passed for your convenience. The basic_deliver object that
+    is passed in carries the exchange, routing key, delivery tag and
+    a redelivered flag for the message. The properties passed in is an
+    instance of BasicProperties with the message properties and the body
+    is the message that was sent.
+    :param pika.channel.Channel _unused_channel: The channel object
+    :param pika.Spec.Basic.Deliver: basic_deliver method
+    :param pika.Spec.BasicProperties: properties
+    :param bytes body: The message body
+    """
+    LOGGER.info('Received message # %s from %s: %s',
+                basic_deliver.delivery_tag, properties.app_id, body)
 
 if __name__ == '__main__':
     try:
--- unpack.py
+++ unpack.py
@@ -2,19 +2,21 @@
 from minio import Minio
 from minio.error import S3Error
 
+
+
 def main():
   # Create a client with the MinIO server playground, its access key
   # and secret key.
   client = Minio(
-    endpoint='minio-chris-d.console-openshift-console.flows-dev-cluster-7c309b11fc78649918d0c8b91bcb5925-0000.eu-gb.containers.appdomain.cloud,
+    endpoint='minio-chris-d.console-openshift-console.flows-dev-cluster-7c309b11fc78649918d0c8b91bcb5925-0000.eu-gb.containers.appdomain.cloud',
     secure=False,
     access_key='RknDK8Uv2psL78hi',
     secret_key='TlEkW1rah7uTtJPuDrh5kdbj2Bd8pxYX'
   )
 
-  bucket_name = ‘input’ 
-  object_name = ‘challenge-text.zip’
-  file_name = ‘/tmp/challenge-text.zip’
+  bucket_name = "input"
+  object_name = "challenge-text.zip"
+  file_name = "/tmp/challenge-text.zip"
   
   found = client.bucket_exists(bucket_name)
   if not found:
@@ -23,6 +25,8 @@ def main():
     print("Bucket already exists")
 
   result = client.fget_object(bucket_name, object_name, file_name)
+  print("file result", result)
+
 
 if __name__ == "__main__":
   try:
