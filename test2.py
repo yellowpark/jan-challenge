e14063a546109805abe18944d85120b9b1249209
Chris Dalby <chris.dalby-cic-uk@uk.ibm.com>
2023-03-21 16:09:00 +0000

    updated publish

---
+++ test2.py
@@ -0,0 +1,39 @@
+import functools
+import logging
+import time
+import pika
+import os
+import json
+
+from dotenv import load_dotenv
+
+from pika.exchange_type import ExchangeType
+
+
+EXCHANGE = 'dw'
+EXCHANGE_TYPE = ExchangeType.topic
+QUEUE = 'unpacker-queue'
+FORMATTER_QUEUE = 'formatter-queue'
+ROUTING_KEY = 'example.text'
+
+# Lodookup environment variables
+load_dotenv()
+
+# Lodookup environment variables
+RABBIT_USER_ENV_VAR = os.getenv('RABBIT_USER')
+RABBIT_PASS_ENV_VAR = os.getenv('RABBIT_PASS')
+
+RABBIT_SERVICE = 'rabbitmq'
+
+message = 'hello, world'
+try:
+    credentials = pika.PlainCredentials(RABBIT_USER_ENV_VAR, RABBIT_PASS_ENV_VAR)
+    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq', credentials=credentials))
+    channel = connection.channel()
+
+    channel.basic_publish(EXCHANGE, FORMATTER_QUEUE, body=message)
+    
+    print(" [x] Sent %r" % message)
+    connection.close()
+except Exception as e:
+    print(f'failed to publish message to broker - {message} - {e}')
